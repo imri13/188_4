db5e8791e4c02c9fb6b28fa1c2f79c8309bcded5
Omer Issachar <omeriss10@gmail.com>
2023-06-12 18:20:50 +0300

    1th commit

---
+++ server.js
@@ -0,0 +1,29 @@
+// import and set up
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const SQL = require('./DB/DB');
+const port = 2002;
+app.use(express.static(path.join(__dirname, "static")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+//routing
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, "views/login.html"));
+});
+
+app.get('/signUp', (req,res)=>{
+    res.sendFile(path.join(__dirname, "views/signUp.html"));
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname, "views/login.html"));
+});
+
+//set up listen
+app.listen(port, ()=>{
+    console.log("server is running on port", port);
+});
---
+++ static/script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Sign Up Button on Login Page
+function signupButtonFunc() {
+    window.location.href = '/signUp';
+}
+
+// Login Button on Sign Up Page
+function loginButtonFunc() {
+    window.location.href = '/login';
+}
