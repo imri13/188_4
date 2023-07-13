d70a9b7381a761225e1d00b719be68a12e7be3c4
Omer Issachar <omeriss10@gmail.com>
2023-07-13 16:57:07 +0300

    part c 3rd commit

--- DB/CRUD.js
+++ DB/CRUD.js
@@ -1,165 +1,28 @@
 const path = require('path');
 const SQL = require('./DB');
-const csv = require('csvtojson');
 
-// Users Table
-const createTableUsers = (req,res)=>{
-    const Q1 = 'CREATE TABLE IF NOT EXISTS `Users` (name varchar(255) NOT NULL, email varchar(255) NOT NULL PRIMARY KEY, password Int NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8';    
-    SQL.query(Q1, (err,mysqlres)=>{
-        //console.log("in query");
+const insertNewFavorite = (req,res)=>{
+    //res.send(req.query);
+    // validate info exists
+
+    // pull info from req.query to json object
+    const NewSignUp = {
+        email: req.query.UserEmail, 
+        placeID: req.query.placeID
+    };
+    // run insert query
+    const Q1 = "INSERT INTO Favorites SET ?";
+    sql.query(Q1, NewSignUp, (err, mysqlres)=>{
         if (err) {
             console.log(err);
-            res.status(400).send(err);
+            res.send("something went wrong");    
             return;
         }
-        res.redirect("/insertUsers");
-    })};
-
-const insertDataUsers = (req,res)=>{
-    const csvPath = path.join(__dirname, "users.csv");
-    /// this is new
-    csv().fromFile(csvPath).then((jsonObj)=>{
-        console.log(jsonObj);
-        for (let i = 0; i < jsonObj.length; i++) {
-            const element = jsonObj[i];
-            console.log(element);
-            const NewCsvData = {
-                name: element.name,
-                email: element.email,
-                password: element.password
-            }; 
-            const Q4 = "insert into Users set ?";
-            SQL.query(Q4, NewCsvData, (err, mysqlres)=>{
-            if (err) {
-                throw err
-            }
-            });
-        }    
-    });
-    res.send("Data inserted into table");
- };
-
-const dropTableUsers = (req,res)=>{
-    const Q2 = 'drop TABLE `Users`;';    
-    SQL.query(Q2, (err,mysqlres)=>{
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-            return;
-        }
-        res.send("hi - table dropped");
+        //res.send("thank you!");
+        //res.sendFile(path.join(__dirname, "../views/search.html"));
+        //res.cookie("nameUser", req.query.UserName);
+        res.redirect("/activUser");
         return;
-    })};
+    });};
 
-// Types Table
-const createTableTypes = (req,res)=>{
-    const Q1 = 'CREATE TABLE IF NOT EXISTS `Types` (typeID Int NOT NULL PRIMARY KEY, typeName varchar(255) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8';    
-    SQL.query(Q1, (err,mysqlres)=>{
-        //console.log("in query");
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-            return;
-        }
-        res.redirect("/insertTypes");
-    })};
-
-const insertDataTypes = (req,res)=>{
-    const csvPath = path.join(__dirname, "types.csv");
-    // this is new
-    csv().fromFile(csvPath).then((jsonObj)=>{
-        console.log(jsonObj);
-        for (let i = 0; i < jsonObj.length; i++) {
-            const element = jsonObj[i];
-            console.log(element);
-            const NewCsvData = {
-                typeID: element.typeID,
-                typeName: element.typeName
-            }; 
-            const Q4 = "insert into Types set ?";
-            SQL.query(Q4, NewCsvData, (err, mysqlres)=>{
-            if (err) {
-                throw err
-            }
-            });
-        }    
-    });
-    res.send("Data inserted into table");
-};
-
-const dropTableTypes = (req,res)=>{
-    const Q2 = 'drop TABLE `Types`;';    
-    SQL.query(Q2, (err,mysqlres)=>{
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-            return;
-        }
-        res.send("hi - table dropped");
-        return;
-    })};
-
-// Areas Table
-const createTableAreas = (req,res)=>{
-    const Q1 = 'CREATE TABLE IF NOT EXISTS `Areas` (area varchar(255) NOT NULL PRIMARY KEY) ENGINE=InnoDB DEFAULT CHARSET=utf8';    
-    SQL.query(Q1, (err,mysqlres)=>{
-        //console.log("in query");
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-            return;
-        }
-        res.redirect("/insertAreas");
-    })};
-
-const insertDataAreas = (req,res)=>{
-    const csvPath = path.join(__dirname, "areas.csv");
-    // this is new
-    csv().fromFile(csvPath).then((jsonObj)=>{
-        console.log(jsonObj);
-        for (let i = 0; i < jsonObj.length; i++) {
-            const element = jsonObj[i];
-            console.log(element);
-            const NewCsvData = {
-                area: element.area
-            }; 
-            const Q4 = "insert into Areas set ?";
-            SQL.query(Q4, NewCsvData, (err, mysqlres)=>{
-            if (err) {
-                throw err
-            }
-            });
-        }    
-    });
-    res.send("Data inserted into table");
-};
-
-const dropTableAreas = (req,res)=>{
-    const Q2 = 'drop TABLE `Areas`;';    
-    SQL.query(Q2, (err,mysqlres)=>{
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-            return;
-        }
-        res.send("hi - table dropped");
-        return;
-    })};
-
-
-//בינתיים לבדיקות    
-const selectAll = (req,res)=>{
-    const Q5 = 'select * from `Areas`;';    
-    SQL.query(Q5, (err,mysqlres)=>{
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-            return;
-        }
-        res.send(mysqlres);
-        return;
-    })};
-
-module.exports = {createTableUsers, insertDataUsers, dropTableUsers,
-                  createTableTypes, insertDataTypes, dropTableTypes,
-                  createTableAreas, insertDataAreas, dropTableAreas, selectAll};
+module.exports = {insertNewFavorite};
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const CRUD = require('./DB/CRUD');
+const DB_CRUD = require('./DB/CreateDB_CRUD');
 //const SQL = require('./DB/DB');
 const port = 3000;
 app.use(express.static(path.join(__dirname, "static")));
@@ -50,21 +50,40 @@ app.post('/mainForm', (req,res)=>{
 //create DB
 
 //Users table
-app.get('/createTableUsers', CRUD.createTableUsers);
-app.get('/insertUsers', CRUD.insertDataUsers);
-app.get('/dropTableUsers', CRUD.dropTableUsers);
+app.get('/createTableUsers', DB_CRUD.createTableUsers);
+app.get('/insertUsers', DB_CRUD.insertDataUsers);
+app.get('/dropTableUsers', DB_CRUD.dropTableUsers);
 
 //Types table
-app.get('/createTableTypes', CRUD.createTableTypes);
-app.get('/insertTypes', CRUD.insertDataTypes);
-app.get('/dropTableTypes', CRUD.dropTableTypes);
+app.get('/createTableTypes', DB_CRUD.createTableTypes);
+app.get('/insertTypes', DB_CRUD.insertDataTypes);
+app.get('/dropTableTypes', DB_CRUD.dropTableTypes);
 
 //Areas table
-app.get('/createTableAreas', CRUD.createTableAreas);
-app.get('/insertAreas', CRUD.insertDataAreas);
-app.get('/dropTableAreas', CRUD.dropTableAreas);
+app.get('/createTableAreas', DB_CRUD.createTableAreas);
+app.get('/insertAreas', DB_CRUD.insertDataAreas);
+app.get('/dropTableAreas', DB_CRUD.dropTableAreas);
 
-app.get('/selectAll', CRUD.selectAll);
+//Partners table
+app.get('/createTablePartners', DB_CRUD.createTablePartners);
+app.get('/insertPartners', DB_CRUD.insertDataPartners);
+app.get('/dropTablePartners', DB_CRUD.dropTablePartners);
+
+//Places table
+app.get('/createTablePlaces', DB_CRUD.createTablePlaces);
+app.get('/insertPlaces', DB_CRUD.insertDataPlaces);
+app.get('/dropTablePlaces', DB_CRUD.dropTablePlaces);
+
+//PartnersInPlace table
+app.get('/createTablePartnersInPlace', DB_CRUD.createTablePartnersInPlace);
+app.get('/insertPartnersInPlace', DB_CRUD.insertDataPartnersInPlace);
+app.get('/dropTablePartnersInPlace', DB_CRUD.dropTablePartnersInPlace);
+
+//Favorites table
+app.get('/createTableFavorites', DB_CRUD.createTableFavorites);
+app.get('/dropTableFavorites', DB_CRUD.dropTableFavorites);
+
+app.get('/selectAll', DB_CRUD.selectAll);
 
 
 //set up listen
